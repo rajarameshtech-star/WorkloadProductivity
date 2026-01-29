51e7619bb31d3306239e4994477d53f7831a2637
Raja Ramesh M <rajaramesh.tech@gmail.com>
2026-01-29 15:26:20 +0530

    Add project files.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WorkloadProductivity.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Flaming"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
